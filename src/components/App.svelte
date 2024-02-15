5bdc63b3b676a69e6c57975e9b52b7fcf689069f
nick swetlin <nswetlin@ucsd.edu>
2024-02-14 23:56:51 -0800

    Points and X-axis

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,10 +1,25 @@
 <script>
     import * as d3 from 'd3';
+    import { onMount } from 'svelte';
     import Graph from '../components/Graph.svelte';
+
+    let data = [];
+
+    onMount(async() => {
+        const res = await fetch('spotify_top_music.csv'); 
+        const csv = await res.text();
+        data = await d3.csvParse(csv, d3.autoType);
+        data = data;
+        // Remove corrupted data outlier.
+        data.splice(442, 1);
+
+        console.log(data[0].bpm)
+    });
+
 </script>
 
 <main>
-    <Graph />
+    <Graph {data}/>
 </main>
 
 <style>
--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -1,38 +1,56 @@
 <script>
     import * as d3 from 'd3';
-    import { onMount } from 'svelte';
-
-    let data = [];
-
-    onMount(async () => {
-        const res = await fetch(
-          'https://dsc106.com/resources/data/temperatures.csv',
-        );
-        const csv = await res.text();
-        await d3.csvParse(csv, (d, i, columns) => {
-        for (let i = 1; i < 13; ++i) {
-            // pivot longer
-            data.push({
-                date: new Date(Date.UTC(d.Year, i - 1, 1)),
-                value: +d[columns[i]],
-            });
-        }
-        });
-        data = data;
-    });
-
-    console.log(data);
-
-
-    // reader.addEventListener('load', function(e) {   
-    //     let fileContents = e.target.result
-    //     console.log(fileContents)
-    // });
     
-    // data = reader.readAsText(fileObj);
-    // var data = d3.dsvFormat(",").parse("../data/spotify_top_music.csv");
-    // console.log(data);
+    export let data;
+
+    const width = 928;
+    const height = 600;
+    const marginTop = 20;
+    const marginRight = 30;
+    const marginBottom = 30;
+    const marginLeft = 40;
+
+    $: x = d3
+    .scaleUtc()
+    .domain(d3.extent(data, (d) => d.dB))
+    .range([marginLeft, width - marginRight]);
+
+    let tooltipPt = null;
+    function onPointerMove(event) {
+    const i = bisect(data, x.invert(d3.pointer(event)[0]));
+    tooltipPt = data[i];
+    }
+
+    function onPointerLeave(event) {
+    tooltipPt = null;
+    }
+
+    //$: d3.select(svg)
+    // .on('pointerenter pointermove', onPointerMove)
+    // .on('pointerleave', onPointerLeave);
 
 </script>
 
+<div class='plot'>
+    <svg
+    {width}
+    {height}
+    viewBox = "0 0 {width} {height}"
+    style = "max-width: 100%; height:auto"
+    >
+
+    <!-- Point marks -->
+    <g stroke="#000" stroke-opacity="0.2">
+        {#each data as d, i}
+          <circle key={i} cx={x(d.dB)} cy={200} fill={'blue'} r="2.5" />
+        {/each}
+      </g>
+
+      {#if tooltipPt}
+      <g transform="translate({x(tooltipPt.dB)},{y(tooltipPt.dB)})">
+        <text font-weight="bold">{tooltipPt.dB}</text>
+      </g>
+    {/if}
+    </svg>
+</div>
 
